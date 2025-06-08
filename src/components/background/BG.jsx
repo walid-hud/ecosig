bbf0df7a4a860051fcf70b44ad94c4496e76a353
walid-hud <walidthehud@gmail.com>
2025-06-08 21:18:58 +0100

    changed the site's background

--- src/components/background/BG.jsx
+++ src/components/background/BG.jsx
@@ -2,8 +2,8 @@ import './background.css';
 
 const BG = () => {
   return (
-    <div id="background">
-      <img src="./bg1.png" loading="lazy" alt="Background" />
+    <div id="background" className='bg-indigo200 [#b7e2fe]'>
+      {/* <img src="./bg1.png" loading="lazy" alt="Background" /> */}
     </div>
   );
 };
